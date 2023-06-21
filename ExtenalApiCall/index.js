3cd7b07ea9fba3e1d946f4bb9f562d91dcb4f1fd
Janath Wijerathna <janath.wijerathna@corp.nexon.com.au>
2023-06-21 23:28:36 +0530

    External API call

--- ExtenalApiCall/index.js
+++ ExtenalApiCall/index.js
@@ -1,13 +1,37 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const axios = require('axios');
+const apiKey = "3ce431dc7de8d5a65eb0f913d9b61350"; // weather api key (e.g. weatherstack.com)
+const defaultLocationCityCountry = "Toronto, Canada";
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+module.exports = async function (context, req) {
+  const location = req.query.location; // To parse http query string
+
+  context.log(`req location: ${location}`);
+  const locationCityCountry = location || defaultLocationCityCountry;
+
+  // prepare the url for axios to call weatherstack api
+  const url = `http://api.weatherstack.com/current?access_key=${apiKey}&query=${locationCityCountry}`;
+  context.log(`url: ${url}`);
+
+  try {
+    // Initiate http call with weatherstack api
+    const response = await axios.get(url);
+
+    // Handle the http response
+    const json = response.data;
+
+    const responseMessage = json
+      ? `This HTTP triggered function executed successfully! \n weatherstack json response is \n ${JSON.stringify(json)}`
+      : "This HTTP triggered function executed successfully!!";
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+      status: 200,
+      body: responseMessage
     };
-}
+  } catch (error) {
+    context.log.error(error);
+    context.res = {
+      status: 500,
+      body: "An error occurred while executing the function."
+    };
+  }
+};
